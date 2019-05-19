1d001516a34f8d6c066421b9042648c1054a668c
Dominik Fuchs <domi.fuchs@hotmail.de>
2019-05-19 20:30:45 +0200

    short

--- script.py
+++ script.py
@@ -5,9 +5,5 @@ DHT_PIN = 23
 DHT_SENSOR = Adafruit_DHT.DHT11
 
 def get_humidity():
-	humidity = None
-	while humidity is None:
-		humidity, _ = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-		if humidity is None:
-			time.sleep(1.5)
+	humidity, _ = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN, retries=20)
 	return humidity
